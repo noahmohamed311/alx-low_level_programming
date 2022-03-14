f07bfd273058247edc6aa7007db18dee7e218322
noahmohamed311 <noahmohamed311@gmail.com>
2022-03-14 18:14:00 +0300

    print telpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ABC;
+for (ABC = 'z'; ABC >= 'a'; ABC--)
+{
+	putchar (ABC);
+}
+	putchar ('\n');
+return (0);
+}
