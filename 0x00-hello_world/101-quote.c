0645f2ab7e7ac75049a501bcd116373e4a1faaa0
Noah Mohamed <69303159+noahmohamed311@users.noreply.github.com>
2022-03-14 18:51:10 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-    prrintf("and that piece of art is useful".\- Dora Korpar, 2015-10-19,");
+    printf("and that piece of art is useful".\- Dora Korpar, 2015-10-19,");
     return 1;
 }
 
