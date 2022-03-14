ac2c80f4c2051528caeb84ce80752c7de8eecacf
Noah Mohamed <69303159+noahmohamed311@users.noreply.github.com>
2022-03-14 18:54:21 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,10 @@
  *
  * Sizeof: used to compute the size of its operand.
  *
- * Return: Always 0 (For success)
+ * Return:  1 For success
  */
 int main(void)
 {
-    printf("and that piece of art is useful".\- Dora Korpar, 2015-10-19,");
+    printf ("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
     return (1);
 }
-
