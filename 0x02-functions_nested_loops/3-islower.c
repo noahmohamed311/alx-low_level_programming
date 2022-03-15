25d069b49fbf24346b28604608acba158d9e1e4a
noahmohamed311 <noahmohamed311@gmail.com>
2022-03-15 23:21:36 +0300

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - entry point
+ *
+ * Description: Prints the alphabet with _putchar
+ * @c: character to check
+ * Return: void
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+else
+{
+	return (0);
+}
+
+}
