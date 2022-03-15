f3343f6007054bfbfcd0e2eab10a76dcafee0032
noahmohamed311 <noahmohamed311@gmail.com>
2022-03-15 23:24:50 +0300

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ *
+ * Description: Prints the alphabet with _putchar
+ * @n: character to check
+ * Return: void
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	return (n);
+	else if (n < 0)
+	return (-n);
+return (0);
+}
