32e3f4c3846f8a22f344f11d07fccc315fa346f1
noahmohamed311 <noahmohamed311@gmail.com>
2022-03-21 19:59:59 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ *
+ * @a: first variable
+ *
+ * @b: second variable
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i;
+
+i = *a;
+*a = *b;
+*b = i;
+}
