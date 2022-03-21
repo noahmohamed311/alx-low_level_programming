e840ba918325e730f79d1acf674b7492f4b68647
noahmohamed311 <noahmohamed311@gmail.com>
2022-03-21 20:01:10 +0300

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: Point to the string
+ *
+ *
+ *
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+return (i);
+}
