fd3b7d29154ef66be5c304b9a5977cd640a8243c
noahmohamed311 <noahmohamed311@gmail.com>
2022-03-24 17:25:57 +0300

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize words of a string.
+ * @s: pointer to input string.
+ *
+ * Return: Returns pointer to capitalized string.
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}"; /* word seperators */
+
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A'); /* changes first letter of word */
+	while (s[i] != '\0')
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z')) /**
+																	  * checks if element before is word seperator
+																	 */
+				s[i] -= ('a' - 'A'); /* capiatilzes if after seperator */
+		i++;
+	}
+	return (s);
+}
+
