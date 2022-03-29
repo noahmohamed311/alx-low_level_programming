a0764820e0f8536f4371c94017c1d137bda189dd
noahmohamed311 <noahmohamed311@gmail.com>
2022-03-29 08:54:04 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - gets length of prefix substring
+ *
+ * @s: string to check
+ * @accept: bytes to compare
+ *
+ * Return: number of bytes in ititial segment s which
+ * consist only of bytes from accept
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
