edf2ebec87185628963b5a0260f64eedefefefef
Noah Mohamed <69303159+noahmohamed311@users.noreply.github.com>
2022-07-02 00:23:05 +0300

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,12 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#ifndef __HASH_TABLES__H__
+#define __HASH_TABLES__H__
+
+/*---- Included libraries ----*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+/*---- Structs ----*/
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -32,6 +35,8 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+/*---- Prototypes ----*/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -39,4 +44,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-#endif /*HASH_TABLES_H */
+#endif
