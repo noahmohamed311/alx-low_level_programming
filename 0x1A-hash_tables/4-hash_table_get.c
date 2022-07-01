e5dd843837b93713d42fd7e0e7af9d22e2b9651c
Noah Mohamed <69303159+noahmohamed311@users.noreply.github.com>
2022-07-02 00:25:19 +0300

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,32 +1,28 @@
 #include "hash_tables.h"
+
 /**
-* hash_table_get - function
-* @ht: table to search
-* @key: key to retrieve
-*
-* Description: function to get value from a passed key.
-* Return: Value of key or NULL if fail
-*/
+ * hash_table_get - Retrieves a value assocaited with a key
+ * @ht: The hash table to look into
+ * @key: The key to look for
+ * Return: The value associated with the element, or NULL if key can't be found
+ */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *tempNode;
-	unsigned long int index;
+	hash_node_t *ptr;
+	unsigned long int idx;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
-	if (key == NULL || (strcmp(key, "") == 0))
-		return (NULL);
-	index = (hash_djb2((const unsigned char *)key) % (ht->size));
-	tempNode = ht->array[index];
+	idx = key_index((unsigned char *)key, ht->size);
+	ptr = ht->array[idx];
 
-	while (tempNode)
+	while (ptr != NULL)
 	{
-		if (strcmp(key, tempNode->key) == 0)
-		{
-			return (tempNode->value);
-		}
-		tempNode = tempNode->next;
+		if (strcmp(ptr->key, key) == 0)
+			return (ptr->value);
+		ptr = ptr->next;
 	}
 	return (NULL);
 }
